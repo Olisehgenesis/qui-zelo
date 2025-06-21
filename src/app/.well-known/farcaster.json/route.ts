8319091499b78058cfa109ef834b78b34de39812
Oliseh <studyug@hotmail.com>
2025-06-21 21:48:17 +0300

    aasociated the app

--- src/app/.well-known/farcaster.json/route.ts
+++ src/app/.well-known/farcaster.json/route.ts
@@ -5,13 +5,13 @@ export async function GET() {
   // The .well-known/farcaster.json route is used to provide the configuration for the Frame.
   // You need to generate the accountAssociation payload and signature using this link:
 
+  
   const config = {
-    accountAssociation: {
-      header:
-        "",
-      payload: "",
-      signature:
-        "",
+    "accountAssociation": {
+      "header": "eyJmaWQiOjgxMDc4MiwidHlwZSI6ImN1c3RvZHkiLCJrZXkiOiIweEVhMzg3NzJCN2I2MjVGODY1M0MwMzQxNzk3QjE3MUUzMDI5RTEzMjUifQ",
+      "payload": "eyJkb21haW4iOiJxdWl6ZWxvLnZlcmNlbC5hcHAifQ",
+      "signature": "MHhlZjE0MDMwOWMzOWI5MDM3ZGRhMDk2NjE4MmZmZDI4MGU1MDA5ZTYzNThkNDMyMWQyNjllOWYwOGZkYzBiYjVlNTMwN2NjOGU1NjhlZmMzMzFiMjQzNjc3N2IwMGIxNjE3ZGY4YjNjOGU2YzY3MDhjN2I4MTAzYmI3ZGQ3OWRlNzFi"
+    
     },
     frame: {
       version: "1",
