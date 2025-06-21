a0432cd527a86a2d375baf62ec2edd319f8c47fb
Oliseh <studyug@hotmail.com>
2025-06-21 21:49:31 +0300

    removed nevt studff

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,15 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  async redirects() {
-    return [
-      {
-        source: '/.well-known/farcaster.json',
-        destination: 'https://api.farcaster.xyz/miniapps/hosted-manifest/0197934f-7064-614b-b08f-52fc06844b94',
-        permanent: false
-      }
-    ]
-  }
-};
+// // const nextConfig: NextConfig = {
+// //   // async redirects() {
+// //   //   return [
+// //   //     {
+// //   //       source: '/.well-known/farcaster.json',
+// //   //       destination: 'https://api.farcaster.xyz/miniapps/hosted-manifest/0197934f-7064-614b-b08f-52fc06844b94',
+// //   //       permanent: false
+// //   //     }
+// //   //   ]
+// //   // }
+// // };
 
-export default nextConfig;
+// export default nextConfig;
